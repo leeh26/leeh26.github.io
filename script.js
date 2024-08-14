708be13ddd3f2feddb5c0a8e520c2d12f1d729ed
leeh26 <111372562+leeh26@users.noreply.github.com>
2024-08-14 09:44:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function updateDateTime() {
+    const now = new Date();
+    const monthoptions = { month: 'long' };
+    const dayoptions = { day: 'numeric'};
+    const hroptions = { hour: '2-digit', minute: '2-digit' };
+    document.getElementById('date-month').innerText = now.toLocaleString('en-US', monthoptions);
+    document.getElementById('date-day').innerText = now.toLocaleString('en-US', dayoptions);
+    document.getElementById('date-hr').innerText = now.toLocaleString('en-US', hroptions);
+}
+
+setInterval(updateDateTime, 10000);
+updateDateTime();
