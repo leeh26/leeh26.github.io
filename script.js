12d37fa36fe47b1e51f0d41c2f2cebb101de0bd3
leeh26 <111372562+leeh26@users.noreply.github.com>
2024-08-14 11:20:12 -0400

    Update script.js
    change to 24 hour clock

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function updateDateTime() {
     const now = new Date();
     const monthoptions = { month: 'long' };
     const dayoptions = { day: 'numeric'};
-    const hroptions = { hour: '2-digit', minute: '2-digit' };
+    const hroptions = { hour: '2-digit', minute: '2-digit', hour12:false };
     document.getElementById('date-month').innerText = now.toLocaleString('en-US', monthoptions);
     document.getElementById('date-day').innerText = now.toLocaleString('en-US', dayoptions);
     document.getElementById('date-hr').innerText = now.toLocaleString('en-US', hroptions);
